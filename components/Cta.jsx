22403a9d614a926fa1111bf4ab13543d5bcf75a3
MAKANAKI <chidoziemail@gmail.com>
2024-04-11 10:57:49 +0100

    footer2

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -4,10 +4,13 @@ import Link from 'next/link'
 
 const Cta = () => {
   return (
-    <section>
+    <section className=' py-24 bg-tertiary dark:bg-secondary '>
         <div className="container mx-auto">
             <div className="flex flex-col items-center">
-                <h2>Prepared to turn your ideas into reality</h2>
+                <h2 className='h2 max-w-xl text-center mb-8'>Prepared to turn your ideas into reality? I am here help
+                  
+                </h2>
+                <Link href='/'><Button>Contact me</Button></Link>
             </div>
         </div>
     </section>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Social from './Social'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
+    <footer>
+      <div className="container mx-auto ">
+        <div className="flex flex-col items-center justify-between ">
+          {/* social */}
+          <Social containerStyle="flex gap-x-6 mx-auto xl:mx-0 mb-4 " 
+          iconsStyles=" text-primary dark:text-white/70 hover:text-white dark:hover:text-primary" />
+          {/* copyright */}
+          <div className="text-muted-foreground">
+            Copyright &copy; Dozie Umeodinka. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer

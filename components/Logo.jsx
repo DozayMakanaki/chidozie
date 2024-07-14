7ed5b07aeff4816b15b70e3889899fe3f3922d45
MAKANAKI <chidoziemail@gmail.com>
2024-07-14 02:04:23 +0100

    new commit

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -6,7 +6,7 @@ import LogoImage from "../public/logo.png";
 const logo = () => {
   return (
     <Link href="/">
-      <Image src={LogoImage} alt="Logo" className="bg-primary rounded-full" width={54} height={54} priority />
+      <Image src={LogoImage} alt="Logo" className="bg-primary rounded-full" width={50} height={50} priority />
     </Link>
   );
 };
